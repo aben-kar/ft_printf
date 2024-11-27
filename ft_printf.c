8650fc7c8c1c1d79d47ecb571c1e63088bb2e8b6
Achraf Ben Kardoud <acben-ka@e2r2p10.1337.ma>
2024-11-27 14:47:28 +0100

    pri

--- ft_printf.c
+++ ft_printf.c
@@ -1,5 +1,19 @@
 #include "libftprintf.h"
 
+int format(va_list args, char c)
+{
+    int count;
+
+    count = 0;
+    if (c == 'c')
+        count += ft_putchar(va_arg(args, int));
+    else if (c == 's')
+        count += ft_putstr(va_arg(args, char *));
+    else if (c == 'd' || c == 'i')
+        count += ft_putnbr(va_arg(args, int));
+    else if (c == 'x' || c == 'X')
+        count += ft_putuhex(va_arg(args, int));
+}
 int ft_printf(const char *format, ...)
 {
     va_list args;
--- ft_putchar.c
+++ ft_putchar.c
@@ -19,4 +19,26 @@ int ft_putstr(char *str)
         i++;
     }
     return len;
+}char	*ft_strchr(const char *str, int c)
+{
+	if (!str)
+		return (NULL);
+	while (*str)
+	{
+		if ((char)c == *str)
+			return ((char *)str);
+		str++;
+	}
+	if ((char)c == '\0')
+		return ((char *)str);
+	return (NULL);
+}
+int check_fromat(char f)
+{
+    char *find;
+
+    find = "csdiupxX%";
+    if (ft_strchr(find, f))
+        return (1);
+    return (0);
 }
